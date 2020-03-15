3d61652f06c03a4b18e8530983f7987fca7cb34f
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-03-15 15:53:40 +0200

    first commit

---
+++ src/Animals.js
@@ -0,0 +1,39 @@
+import chicken from "./images/chicken.png";
+import dog from "./images/Dog.png";
+import cow from "./images/cow.jpg";
+import mikasa from "./images/mikasa.png";
+import knafeh from "./images/knafeh.jpg";
+import hammam from "./images/Hamam.jpg";
+const animals = [
+  {
+    name: "Chicken",
+    imageUrl: chicken,
+    lost: "You Lost to a chicken.."
+  },
+  {
+    name: "Dog",
+    imageUrl: dog,
+    lost: "You Lost,but dont worry, 100% worthy"
+  },
+  {
+    name: "Cow",
+    imageUrl: cow,
+    lost: "You have your mouth watering, no blame for the loss "
+  },
+  {
+    name: "Hammam",
+    imageUrl: hammam,
+    lost: "You Lost,to a Russian Mafioso.."
+  },
+  {
+    name: "Mikasa",
+    imageUrl: mikasa,
+    lost: "EREEEEEEEEEEEEEEEEEEEEEN!!"
+  },
+  {
+    name: "Knafeh",
+    imageUrl: knafeh,
+    lost: "You lost, now you buy US some\nand by us  i mean : Me , Myself and I"
+  }
+];
+export default animals;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import animals from "./Animals";
+class App extends Component {
+  state = {
+    animal: animals[0],
+    i: 0
+  };
+  getAnimal = () => {
+    let y = this.state.i + 1 === animals.length ? 0 : this.state.i + 1;
+    this.setState({
+      animal: animals[y],
+      i: y
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="d-flex flex-column bd-highlight mb-3">
+          <div className="p-2 bd-highlight justify-content-center">
+            <h1>dont look at the {this.state.animal.name}</h1>
+          </div>
+          <div className="p-2 bd-highlight justify-content-center">
+            <img
+              className="w-25"
+              src={this.state.animal.imageUrl}
+              alt="chicken"
+            />
+          </div>
+          <div className="p-2 bd-highlight justify-content-center">
+            <h2 style={{ color: "red" }}>{this.state.animal.lost}</h2>
+          </div>
+          <div className="p-2 bd-highlight justify-content-center">
+            <button
+              type="button"
+              class="btn btn-warning"
+              onClick={() => this.getAnimal()}
+            >
+              Play another Game
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
